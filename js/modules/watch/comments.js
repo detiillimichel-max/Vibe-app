6a1817353f192d58072df69a7cb86e4701f383ff
Michel Detiilli <detiillimichel@gmail.com>
2026-03-28 12:54:06 -0300

    Create comments.js

---
+++ js/modules/watch/comments.js
@@ -0,0 +1,8 @@
+/**
+ * OIO ONE - COMMENT SYSTEM 💬
+ */
+window.CommentSystem = {
+    openComments(videoId) {
+        alert("OIO ONE: Abrindo central de comentários para o vídeo " + videoId);
+    }
+};
