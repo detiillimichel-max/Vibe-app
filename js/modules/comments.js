b8da8b5011465805fc39c609ee4c735b1cd6f0aa
Michel Detiilli <detiillimichel@gmail.com>
2026-03-28 12:06:51 -0300

    Update comments.js

--- js/modules/comments.js
+++ js/modules/comments.js
@@ -1,1 +1,10 @@
-
+/**
+ * OIO ONE - COMMENT SYSTEM 💬
+ */
+export const CommentSystem = {
+    openComments(videoId) {
+        // Por enquanto, apenas um alerta de luxo para validar a conexão
+        alert(`Abrindo comentários do vídeo: ${videoId}`);
+        // Futuramente, aqui injetaremos a lista do Supabase
+    }
+};
