2852410686a00dfa6a28cd032ca0d297e6bbcc9c
Michel Detiilli <detiillimichel@gmail.com>
2026-03-28 12:54:52 -0300

    Delete js/modules/comments.js

--- js/modules/comments.js
+++
@@ -1,9 +0,0 @@
-/**
- * OIO ONE - COMMENT SYSTEM 💬
- */
-export const CommentSystem = {
-    openComments(videoId) {
-        // Por enquanto, um alerta de luxo. No futuro, abriremos o banco de dados.
-        alert("OIO ONE: Abrindo central de comentários para o vídeo " + videoId);
-    }
-};
