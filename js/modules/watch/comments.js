01511758351b6392e8d491c2f5237cce63163566
Michel Detiilli <detiillimichel@gmail.com>
2026-04-04 17:08:05 -0300

    Delete js directory

--- js/modules/watch/comments.js
+++
@@ -1,8 +0,0 @@
-/**
- * OIO ONE - COMMENT SYSTEM 💬
- */
-window.CommentSystem = {
-    openComments(videoId) {
-        alert("OIO ONE: Abrindo central de comentários para o vídeo " + videoId);
-    }
-};
--- js/services/logger.js
+++
@@ -1,4 +0,0 @@
-export const Logger = {
-    info: (msg) => console.log(`%c[OIO-INFO]: ${msg}`, "color: #d4af37"),
-    error: (msg) => console.error(`%c[OIO-ERROR]: ${msg}`, "color: #ff4b2b")
-};
