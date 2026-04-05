b170f0320a1e36587a810f58dd9c706b29171596
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 08:21:38 -0300

    Create actionBar.js

---
+++ bloco-3-friends/components/actionBar.js
@@ -0,0 +1,15 @@
+function ActionBar(friendId) {
+    return `
+        <div class="action-bar">
+            <button class="btn-action">
+                <i class="fa-solid fa-user-plus"></i> Adicionar
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-message"></i> Mensagem
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-user-xmark"></i> Remover
+            </button>
+        </div>
+    `;
+}
