0bcd58695462dc9928968a960fb58983459eaad0
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 08:30:05 -0300

    Create actionBar.js

---
+++ bloco-4-market/components/actionBar.js
@@ -0,0 +1,15 @@
+function ActionBar(productId) {
+    return `
+        <div class="action-bar">
+            <button class="btn-action">
+                <i class="fa-solid fa-cart-shopping"></i> Comprar
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-heart"></i> Favorito
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-share"></i> Compartilhar
+            </button>
+        </div>
+    `;
+}
