4522ae38e1fcc8ca24a4bad3e7197a0da9c05039
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 09:36:56 -0300

    Update quantum.js

--- js/quantum.js
+++ js/quantum.js
@@ -1,9 +1,16 @@
 // quantum.js - O Corredor
 function abrirBloco(nome) {
-  // animação de transição suave
-  document.body.classList.add('fade-out');
+  const container = document.getElementById('app');
+  container.classList.add('fade-out');
+
   setTimeout(() => {
-    window.location.href = `/${nome}`;
+    fetch(`/bloco-${nome}/index.html`)
+      .then(res => res.text())
+      .then(html => {
+        container.innerHTML = html;
+        container.classList.remove('fade-out');
+        container.classList.add('fade-in');
+      });
   }, 300);
 }
 
