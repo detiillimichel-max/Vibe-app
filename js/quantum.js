57ed5cc3ffac3c3533a9db7466ffa9757c3209a4
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 10:13:19 -0300

    Update quantum.js

--- js/quantum.js
+++ js/quantum.js
@@ -1,19 +1,20 @@
-// quantum.js - O Corredor
-function abrirBloco(nome) {
+// quantum.js - Corredor
+export function abrirBloco(nome) {
   const container = document.getElementById('app');
   container.classList.add('fade-out');
 
   setTimeout(() => {
+    // Cada bloco é uma pasta com index.html
     fetch(`/bloco-${nome}/index.html`)
       .then(res => res.text())
       .then(html => {
         container.innerHTML = html;
         container.classList.remove('fade-out');
         container.classList.add('fade-in');
+      })
+      .catch(err => {
+        container.innerHTML = `<p style="color:red;">Erro ao carregar bloco ${nome}</p>`;
+        console.error(err);
       });
   }, 300);
-}
-
-function fecharBloco() {
-  window.history.back();
 }
