cdf5da0a9cb8ee241f074583575303f73d046a62
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 10:58:08 -0300

    Update quantum.js

--- js/quantum.js
+++ js/quantum.js
@@ -1,14 +1,11 @@
-// quantum.js - Corredor estilo app
 export function abrirBloco(nome) {
   const container = document.getElementById('app');
   container.classList.add('fade-out');
 
   setTimeout(() => {
-    // Cada bloco é uma pasta com index.html
-    fetch(`/bloco-${nome}/index.html`)
+    fetch(`./bloco-${nome}/index.html`)
       .then(res => res.text())
       .then(html => {
-        // Carrega o bloco como tela cheia
         container.innerHTML = `<div class="tela">${html}</div>`;
         container.classList.remove('fade-out');
         container.classList.add('fade-in');
