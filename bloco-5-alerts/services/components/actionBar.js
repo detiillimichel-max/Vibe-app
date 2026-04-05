3b5d589ce5fdab221c9c237bf9bd46828c382167
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 08:39:40 -0300

    Create actionBar.js

---
+++ bloco-5-alerts/services/components/actionBar.js
@@ -0,0 +1,15 @@
+function ActionBar(alertId) {
+    return `
+        <div class="action-bar">
+            <button class="btn-action">
+                <i class="fa-solid fa-eye"></i> Ver
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-trash"></i> Excluir
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-share"></i> Compartilhar
+            </button>
+        </div>
+    `;
+}
