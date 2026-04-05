efe78be7fbdd268fc56a4ebfb8095a5fef025fd0
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 08:53:38 -0300

    Create actionBar.js

---
+++ bloco-6-menu/components/actionBar.js
@@ -0,0 +1,15 @@
+function ActionBar(itemId) {
+    return `
+        <div class="action-bar">
+            <button class="btn-action">
+                <i class="fa-solid fa-gear"></i> Configurações
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-user"></i> Perfil
+            </button>
+            <button class="btn-action">
+                <i class="fa-solid fa-right-from-bracket"></i> Sair
+            </button>
+        </div>
+    `;
+}
